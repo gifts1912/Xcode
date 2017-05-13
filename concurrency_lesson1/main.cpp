75f41576098ac0d2a06a3be8d65b1108f8bea5e6
FrankLiu <frankliu@FrankLiudeMacBook-Pro.local>
2017-05-13 14:44:56 +0800

    Concurrency lesson1: std::thread, std::thread.join()

--- concurrency_lesson1/main.cpp
+++ concurrency_lesson1/main.cpp
@@ -7,9 +7,22 @@
 //
 
 #include <iostream>
+#include <thread>
+#include <vector>
+
+void thread_function(){
+    std::cout << "thread_function called!\n";
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    std::vector<std::thread> workers;
+    for(int i = 0; i < 10; i++){
+        workers.push_back(std::thread(thread_function));
+    }
+    for(std::vector<std::thread>::iterator iter = workers.begin(); iter != workers.end(); iter++){
+        iter->join();
+    }
     std::cout << "Hello, World!\n";
     return 0;
 }
