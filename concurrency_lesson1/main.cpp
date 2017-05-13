bfc8972eda5cb1ece63fe534e2b5476a5464e399
FrankLiu <frankliu@FrankLiudeMacBook-Pro.local>
2017-05-13 15:08:31 +0800

    lambda function ; std::for_each() loop

--- concurrency_lesson1/main.cpp
+++ concurrency_lesson1/main.cpp
@@ -9,20 +9,32 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <sstream>
 
-void thread_function(){
-    std::cout << "thread_function called!\n";
+void thread_function(int i){
+    std::ostringstream oss;
+    oss << "thread_function called!" << i << "\n";
+    std::cout << oss.str();
 }
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    std::ostringstream oss;
     std::vector<std::thread> workers;
     for(int i = 0; i < 10; i++){
-        workers.push_back(std::thread(thread_function));
+        //workers.push_back(std::thread(thread_function, i));
+        workers.push_back(std::thread([i](){
+            std:: cout << "thread lambda function " << i << " \n";
+        }));
     }
+    /*
     for(std::vector<std::thread>::iterator iter = workers.begin(); iter != workers.end(); iter++){
         iter->join();
-    }
+    }*/
+    for_each(workers.begin(), workers.end(), [](std::thread &th){
+        th.join();
+    });
     std::cout << "Hello, World!\n";
     return 0;
 }
+
