781ca604d96124a234f5e2a43f73031704f118a6
gifts1912 <hengyliu@hotmail.com>
2017-05-23 22:25:39 +0800

    global variable can't change
    memset(buf, '\0', sizeof(but)); strcpy(char *des, char*source); strncpy(); strcat(); strncat();

--- concurrency_lesson1/stringProcess.cpp
+++ concurrency_lesson1/stringProcess.cpp
@@ -1,45 +1,139 @@
-/*************************************************************************
-	> File Name: log_2.cpp
-	> Author: ma6174
-	> Mail: ma6174@163.com 
-	> Created Time: Tue 23 May 2017 04:45:43 PM DST
- ************************************************************************/
-
-#include<iostream>
-#include <boost/algorithm/string.hpp>
-#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
 
 using namespace std;
 
-int main(){
-	string str_tmp = "aaa\thello world!\t123\tdd";
-	cout << "original:" << str_tmp << ":" << endl;
-	boost::trim_right(str_tmp);
-	boost::trim_left(str_tmp);
-	cout << "Trim_right:" << str_tmp << ":" << endl;
-	vector<string> vec;
-	boost::split(vec, str_tmp, boost::is_any_of("\t"));
-	for(vector<string>::iterator iter = vec.begin(); iter != vec.end(); iter++){
-		cout << *iter << " : ";
-	}
-	cout << endl;
-	
-	return 0;
+char  g_buf[100];
+
+int getCount(char * ptr, char * pos, int *num){
+    char *v = ptr;
+    if (v == NULL || pos == NULL){
+        return -1;
+    }
+    int i = 0, j = strlen(v) - 1;
+    while(v[i] != '\0' && isspace(v[i])){
+        i++;
+    }
+    while(j >= 0 && isspace(v[j])){
+        j--;
+    }
+    * num = j - i + 1;
+    strncpy(pos, ptr + i, *num);
+    pos[*num] = '\0';
+    return 0;
 }
 
-/*
-	tring line("test\ttest2\ttest3");
-	vector<string> strs;
-	boost::split(strs,line,boost::is_any_of("\t"));
-	
-	cout << "* size of the vector: " << strs.size() << endl;
-	for (vector<string>::iterator it = strs.begin(); it != strs.end(); ++it)
-	{
-	    cout << *it << endl;
-		}""""))"")out << "original: " << str_tmp << endl;
-#include <boost/algorithm/string.hpp>
+int trim(char *v){
+    if (v == NULL){
+        return -1;
+    }
+    int i = 0, j = strlen(v) - 1;
+    while(v[i] != '\0' && isspace(v[i])) {
+        i++;
+    }
+    while(j >= 0 && isspace(v[j])) {
+        j--;
+    }
+    memmove(v, v + i, j - i + 1); // error if v point to global memeory chunk, because the variable in gloabl can't be writed.
+    v[j -i + 1] = '\0';
+    return 0;
+}
 
- std::string str("hello world! ");
- boost::trim_right(str);"")
-	cout << "original: " << str_tmp << endl;
- */
+int trimCall(){
+    char* v = (char*) "  aabbcd  ";
+    char *res = NULL;
+    res = (char *) malloc(100);
+    int num = 0;
+    //getCount(v, res, &num);
+    char buf[] = " aabbcc  ";
+    printf("beforeBuf:%s:end\n", buf);
+    trim(buf); // trim(v) will cause busy error 10; as varaiable in global memory chunk cann't be write.
+    printf("afterBuf:%s:end\n", buf);
+    return 0;
+}
+
+int Inverse(char *v){
+    if (v == NULL){
+        return -1;
+    }
+    int i = 0, j = strlen(v) - 1;
+    while( i < j ){
+        char c = v[i];
+        v[i] = v[j];
+        v[j] = c;
+        i++;
+        j--;
+    }
+    return 0;
+}
+int Inverse02(char *v){
+    if (v == NULL){
+        return -1;
+    }
+    char *b = NULL, *e = NULL;
+    int len = strlen(v);
+    b = v;
+    e = v + len - 1;
+    while( b < e){
+        char c = *b;
+        *b = *e;
+        *e = c;
+        b++;
+        e--;
+    }
+    return 0;
+}
+
+int Inverse03(char *v){
+    if (v == NULL) {
+        return -1;
+    }
+    if (*(v + 1) != '\0'){
+        Inverse03(v+1);
+    }
+    printf("%c", *v);
+    return 0;
+}
+
+int Inverse04(char *v){
+    if (v == NULL) {
+        return -1;
+    }
+    if (*(v + 1) != '\0'){
+        Inverse04(v+1);
+    }
+    strncat(g_buf, v, 1); // strcat(g_buf, v); will copy all v pointer char * to g_buf;
+    return 0;
+}
+
+int Inverse05(char *v, char *buf){
+    if (v == NULL || buf == NULL) {
+        return -1;
+    }
+    if (*v == '\0') {
+        return 0;
+    }
+    Inverse05(v + 1, buf);
+    strncat(buf, v, 1);
+    return 0;
+}
+
+int main(){
+    char t[10];
+    memset(t, '-', sizeof(t));
+    t[sizeof(t) - 1] = '\0';
+    printf("t1:%s\n",t);
+    memset(t, 0, sizeof(t));
+    printf("t2:%s:\n", t);
+    memset(t, '\0', sizeof(t));
+    printf("t3:%s:\n",t);
+    memset(g_buf, '\0', sizeof(g_buf));
+    char v[] = "abcde";
+    char buf[1024];
+    memset(buf, '\0', sizeof(buf));
+    printf("v:%s\n", v);
+    Inverse05(v, buf);
+    printf("inversev:%s\n", buf);
+    return 0;
+}
