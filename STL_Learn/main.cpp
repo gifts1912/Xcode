ced59adfd8b4662b6abc3670608c7da868138a24
gifts1912 <hengyliu@hotmail.com>
2017-05-27 23:30:09 +0800

    Add files via upload
    compare;substr;find;rfind;insert;erase;

--- STL_Learn/main.cpp
+++ STL_Learn/main.cpp
@@ -8,11 +8,13 @@
 
 #include <iostream>
 #include <string>
+#include <cstring>
+#include <clocale>
 
 using namespace std;
 
 
-int main(int argc, const char * argv[]) {
+int string_prePart(int argc, const char * argv[]) {
     // insert code here...
     string s1(2, '1');
     string s2("abce");
@@ -50,6 +52,70 @@ int main(int argc, const char * argv[]) {
     cout << "s3: " << s3 << endl;
     s3.assign(3, 'c');
     cout << "s3: " << s3 << endl;
+    int len = s3.length();
+    cout << "len : " << len << endl;
+    cout << "s3.empty() " << s3.empty() << endl;
     std::cout << "Hello, World!\n";
+    
+    
+    return 0;
+}
+
+int main() {
+    string strA ("UIPower");
+    string strB("Hello World ");
+    strB += strA;
+    strB += '!';
+    strB.append(" 123");
+    strB.append("abcd", 2);
+    strB.append("abcd", 2, 1);
+    
+    string strC("STLTest");
+    strB.append(4, 'U');
+    
+        cout << strB << endl;
+    int a = strB.compare(strC);
+    cout << "strB.compare(strC) " << a << endl;
+    cout << strC.compare(strB) << endl;
+    
+    cout << strA.substr(2, 5) << endl;
+    
+    size_t pos = strA.find("Po", 0);
+    cout << "pos: " << pos << endl;
+    cout << strA.find('P', 0) << endl;
+    string strF = "Po";
+    cout << strA.find(strF, 0) <<endl;
+    cout << strA.substr(pos, strA.length() - pos + 1) << endl;
+    
+    if (strA.find("aa", 0) == -1){
+        cout << "not found" << endl;
+    }
+    else {
+        cout << "found" << endl;
+    }
+    printf("%d\n", strA.find("aa"));
+    
+    pos = strA.find("ow", 0);
+    cout << pos << endl;
+    
+    strA.insert(1, "a");
+    strA.insert(5, "a");
+    cout << strA << endl;
+    strA.insert(strA.length(), 5, 'I');
+    cout << strA << "\tstrA.find('a', 0)" << strA.find("a", 0) <<'\t' <<  strA.rfind("a", strA.length()) << endl;
+    
+    strA.erase(2, 4);
+    cout << strA << endl;
+    
+    strA.replace(2, strA.length() - 2 + 1, "A");
+    cout << strA << endl;
+    
+    string strE = "UIPower";
+    strF = "Hello";
+    cout << strE << '\t' << strF << endl;
+    strE.swap(strF);
+    cout << strE << '\t' << strF << endl;
+    
+    
     return 0;
 }
